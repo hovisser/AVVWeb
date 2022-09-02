79d8679fb5d2d0a19da6ac10b0ed063fc2981377
Hotze Visser <hvisser@MacBook-Pro-van-Hotze.local>
2022-09-02 14:16:48 +0200

    initial serup

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container py-4 px-3 mx-auto">
+      <h1>Hello, Bootstrap and Vite!</h1>
+      <button class="btn btn-primary">Primary button</button>
+    </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
